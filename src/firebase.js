c9b88fdfecb8f67332091c7d76d07dfa9d7abe13
Hawaa <hawaazaroual@gmail.com>
2022-03-10 20:53:44 -0700

    first commit with nav bar and all files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAA8GaazIYLKJ1Mp32iwh_b8PmWEjEL97o",
+  authDomain: "challenge-4141a.firebaseapp.com",
+  projectId: "challenge-4141a",
+  storageBucket: "challenge-4141a.appspot.com",
+  messagingSenderId: "103953191913",
+  appId: "1:103953191913:web:50846d9826e6b3d61fb178",
+  measurementId: "G-G7GEYP1J4L"
+};
