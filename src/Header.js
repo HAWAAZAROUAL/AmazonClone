bb2a72fdb0410a7da4094d4e27396f24398404ff
Hawaa <hawaazaroual@gmail.com>
2022-03-12 16:28:42 -0700

    set up homepage layout

--- src/Header.js
+++ src/Header.js
@@ -22,34 +22,27 @@ function Header() {
       <div className='header__nav'>
         
         <div className='header__option'>
-          <span className='header__optionLineOne'> Hello Guest</span>
-
+          <span className='header__optionLineOne'>Hello Guest</span>
+<span className='header__optionLineTwo'>Sign In</span>
         </div>
         
-        <div className='header__option'>
-          <span className='header__optionLineTwo'>Sign In</span>
+        
           
-          </div>
-         
+          
         <div className='header__option'>
           <span className='header__optionLineOne'>Returns</span>
+           <span className='header__optionLineTwo'>& Orders</span>
           </div>
 
-          <div className='header__option'>
-          <span className='header__optionLineTwo'>& Orders</span>
-          </div>
-
+        
           <div className='header__option'>
           <span className='header__optionLineOne'>Your</span>
-          </div>
-
-          <div className='header__option'>
           <span className='header__optionLineTwo'>Prime</span>
           </div>
 
           <div className='header__optionBasket'>
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo" className='header__basketCount'>0</span>
+            <span className='header__optionLineTwo header__basketCount'>0</span>
             </div>
 
       </div>
